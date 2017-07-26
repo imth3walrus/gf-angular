7b2d56390b5027425e93b232bfa0cb5592fb5133
Juan Duque <sebastian@byafro.com>
2017-07-25 21:41:42 -0400

    finished child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,22 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { MatchesComponent } from './matches/matches.component';
+import { BillingComponent } from './billing/billing.component';
+import { DisputesComponent } from './disputes/disputes.component';
+import { LeaderboardsComponent } from './leaderboards/leaderboards.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MymatchesComponent } from './mymatches/mymatches.component';
+import { TournamentsComponent } from './tournaments/tournaments.component';
 
 const defaultRoutes: Routes = [
   { path: '', redirectTo: 'matches', pathMatch: 'full' },
   { path: 'matches', component: MatchesComponent },
+  { path: 'my-matches', component: MymatchesComponent },
+  { path: 'tournaments', component: TournamentsComponent },
+  { path: 'leaderboards', component: LeaderboardsComponent },
+  { path: 'billing', component: BillingComponent },
+  { path: 'disputes', component: DisputesComponent },
+  { path: 'messages', component: MessagesComponent }
 ]
 
 const routes: Routes = [
